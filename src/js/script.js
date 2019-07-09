65852035788f3e909d815b2028249a0b99c71856
Monika Jung-Wrobel <monikajung84@gmail.com>
2019-07-09 22:00:36 +0200

    Add a quantity widget

--- src/js/script.js
+++ src/js/script.js
@@ -171,6 +171,7 @@
         basePrice = countThePrice(basePrice, 'crust');
         setImages ('crust');
       }
+      basePrice *= thisProduct.amountWidget.value;
       thisProduct.priceElem = basePrice;
       //
       thisProduct.element.querySelector(select.menuProduct.priceElem).innerHTML =thisProduct.priceElem;
@@ -194,8 +195,10 @@
 
     initAmountWidget(){
       const thisProduct = this;
-
       thisProduct.amountWidget = new AmountWidget(thisProduct.amountWidgetElem);
+      thisProduct.amountWidgetElem.addEventListener('updated', function(){
+        thisProduct.processOrder();
+      });
     }
   }
 
@@ -203,9 +206,11 @@
     constructor(element){
       const thisWidget = this;
       thisWidget.getElements(element);
+      thisWidget.value = settings.amountWidget.defaultValue;
       thisWidget.setValue(thisWidget.input.value);
       console.log('AmountWidget: ', thisWidget);
       console.log('constructor arguments: ', element);
+      thisWidget.initActions();
     }
     getElements(element){
       const thisWidget = this;
@@ -217,11 +222,33 @@
     }
     setValue(value){
       const thisWidget = this;
-
       const newValue = parseInt(value);
-      /* TODO: Add validation*/
+      if((value >= settings.amountWidget.defaultMax) && (value <=settings.amountWidget.defaultMin) && (value != newValue)){
       thisWidget.value = newValue;
+      thisWidget.announce();
+      }
       thisWidget.input.value = thisWidget.value;
+    }
+    initActions(){
+      const thisWidget = this;
+      thisWidget.input.addEventListener('change', function(){
+        event.preventDefault();
+        thisWidget.setValue(thisWidget.input.value);
+      });
+      thisWidget.linkDecrease.addEventListener('click', function(){
+        event.preventDefault();
+        thisWidget.setValue(thisWidget.value -1);
+      });
+      thisWidget.linkIncrease.addEventListener('click', function(){
+        event.preventDefault();
+        thisWidget.setValue(thisWidget.value +1);
+      });
+    }
+    announce(){
+      const thisWidget = this;
+
+      const event = new Event('updated');
+      thisWidget.element.dispatchEvent(event);
     }
   }
   const app = {

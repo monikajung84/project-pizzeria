8856a2a78824ec20c8207c84141fc07541c04130
Monika Jung-Wrobel <monikajung84@gmail.com>
2019-07-30 19:32:25 +0200

    Add time and number table

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,7 +1,9 @@
-import {select, templates, classNames} from '../settings.js';
+import {select, templates, settings, classNames} from '../settings.js';
+import {utils} from '../utils.js';
 import {AmountWidget} from './AmountWidget.js';
 import {DatePicker} from './DatePicker.js';
 import {HourPicker} from './HourPicker.js';
+
 
 export class Booking {
   constructor(bookingContainer){
@@ -9,6 +11,7 @@ export class Booking {
     thisBooking.bookingContainer = bookingContainer;
     thisBooking.render(bookingContainer);
     thisBooking.initWidges();
+    thisBooking.getData();
   }
   render(){
     const thisBooking = this;
@@ -27,5 +30,76 @@ export class Booking {
     thisBooking.hoursAmount = new AmountWidget(thisBooking.dom.hoursAmount);
     thisBooking.datePicker = new DatePicker(thisBooking.dom.datePicker);
     thisBooking.hourPicker = new HourPicker(thisBooking.dom.hourPicker);
+  }
+  getData(){
+    const thisBooking = this;
+    const startEndDates = {};
+    startEndDates[settings.db.dateStartParamKey] = utils.dateToStr(thisBooking.datePicker.minDate);
+    startEndDates[settings.db.dateEndParamKey] = utils.dateToStr(thisBooking.datePicker.maxDate);
+    const endDate = {};
+    endDate[settings.db.dateEndParamKey] = startEndDates[settings.db.dateEndParamKey];
+    const params ={
+      booking: utils.queryParams(startEndDates),
+      eventsCurrent: settings.db.notRepeatParam + '&' + utils.queryParams(startEndDates),
+      eventsRepeat: settings.db.repeatParam + '&' + utils.queryParams(endDate),
+    };
+    console.log('getData params:', params);
+    const urls = {
+      booking: settings.db.url + '/' + settings.db.booking +'?' + params.booking,
+      eventsCurrent: settings.db.url + '/' + settings.db.event + '?' + params.eventsCurrent,
+      eventsRepeat: settings.db.url + '/' + settings.db.event + '?' + params.eventsRepeat,
+    };
+    console.log('getData.urls', urls);
+    Promise.all([
+      fetch(urls.booking),
+      fetch(urls.eventsCurrent),
+      fetch(urls.eventsRepeat),
+    ])
+    .then(function([bookingsResponse, eventsCurrentResponse, eventsRepeatResponse]){
+      return Promise.all([
+        bookingsResponse.json(),
+        eventsCurrentResponse.json(),
+        eventsRepeatResponse.json(),
+      ]);
+    })
+    .then(function([bookings, eventsCurrent, eventsRepeat]){
+      thisBooking.parseData(bookings, eventsCurrent, eventsRepeat);
+    });
+  }
+  parseData(bookings=[], eventsCurrent=[], eventsRepeat=[]){
+    const thisBooking = this;
+    thisBooking.booked = {};
+    console.log('eventsCurrent:', eventsCurrent);
+    for (let element of eventsCurrent){
+      console.log('element:', element);
+      thisBooking.makeBooked(element.date, element.hour, element.duration, element.table);
+    }
+    for (let element of bookings){
+      console.log('element:', element);
+      thisBooking.makeBooked(element.date, element.hour, element.duration, element.table);
+    }
+    for (let element of eventsRepeat){
+      console.log('element:', element);
+      thisBooking.makeBooked(element.date, element.hour, element.duration, element.table);
+    }
+  }
+  makeBooked(date, hour, duration, table){
+    const thisBooking = this;
+    let newObject = {};
+    let time = hour.split (':');
+    for (let elem in time){
+      time[elem]=parseInt(time[elem]);
+    }
+    if (time[1]>=30){
+      hour=time[0]+0.5;
+    }
+    
+    newObject[hour]= [table];
+    hour = time[0] + (duration);
+    newObject[hour] = [table];
+    console.log('time:', time);
+    console.log('newObject:', newObject);
+    thisBooking.booked[date]= newObject;
+
   }
 }
